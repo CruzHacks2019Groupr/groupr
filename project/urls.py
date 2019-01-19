b44eca939446625d8dfd380abe5218be5f64a7a9
Conrad <waterflame99@gmail.com>
2019-01-19 03:40:24 -0800

    login

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     # ex: /notes/5/
     #path('<int:note_id>/', views.detail, name='detail'),
     #path('get_notes/', views.get_notes, name='get_notes'),
