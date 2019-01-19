d4fa25980ad47da75e645f8358e3fb669878ba2a
Kevin Pinney <popocola13@gmail.com>
2019-01-19 03:56:17 -0800

    Update findPerfectGroup.py

--- project/functions/findPerfectGroup.py
+++ project/functions/findPerfectGroup.py
@@ -1,14 +1,18 @@
 #import everything we need
 import networkx as nx
 
-def findPerfectGroup(int) #takes in a UserID and finds if a perfect group exists via DFS
+def newSwipe(ID_1, ID_2) #Update swipe occurs, giving a first and second ID
 
-	#Load userID Have Eric write this part
-	G = nx.Graph()
-	# N = Node of the UserID
-	D = G.to_undirected()
-	List = cliques_containing_node(D, nodes=None)
+	#DiG is digraph from database
+	#G is undirected graph from database
+	DiG.add_edge(ID_1, ID_2)
+	if ID_1 in DiG.neighbors(ID_2)
+		G.add_edge(ID_1, ID_2)
+	List = cliques_containing_node(G, nodes=None)
 	if not List
 		return None;
 	else
+		for i in List[0]:
+			G.remove_node(i)
+			DiG.remove_node(i)
 		return List[0];
