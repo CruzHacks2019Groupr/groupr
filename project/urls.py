b9a31c63c8080158543eaf348214cc6c4df6fa50
Luke Harvey <L@Lukes-MacBook-Pro-3.local>
2019-01-19 04:21:14 -0800

    Added Login and Signup

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^accounts/profile/$', views.redir, name='redir'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^chat/$', views.chat, name='chat'),
     # ex: /notes/5/
     #path('<int:note_id>/', views.detail, name='detail'),
--- project/views.py
+++ project/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.template import loader
@@ -10,18 +10,22 @@ import json
 from .functions import test
 from pusher import Pusher
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
 
 pusher = Pusher(app_id=u'694776', key=u'4105ec1d8d985dcf27bf', secret=u'1cf25393f1f636e8dc3e' ,cluster=u'us2')
 
 def index(request):
-    test.printSomeShit()
     return render(request, 'project/index.html')
-    
+
+def redir(request):
+	return redirect('index');    
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
-#@login_required(login_url='/admin/login/')
+@login_required(login_url='/login/')
 def chat(request):
     return render(request, "project/chat.html");
 
@@ -29,6 +33,21 @@ def chat(request):
 def broadcast(request):
     pusher.trigger(u'a_channel', u'an_event', {u'name': request.user.username, u'message': request.POST['message']})
     return HttpResponse("done");
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
 """
 Demo:
 def post_note(request):
