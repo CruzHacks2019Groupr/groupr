182ffdd9949acd992b542fb01744784ae8297537
Luke Harvey <L@Lukes-MacBook-Pro-3.local>
2019-01-19 06:27:22 -0800

    Last thing of night

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#AUTH_USER_MODEL = 'project.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- project/models.py
+++ project/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+#import networkx as nx
 
 # Create your models here.
 
 class Test(models.Model):
 	count = models.IntegerField(default=0)
+
+#class User(AbstractUser):
+#	bio = models.TextField(max_length=500, blank=True)
+#	birth_date = models.DateField(null=True, blank=True)
+	#pic = models.ImageField(null=True)
+
+#class Event(models.Model):
+#	name = models.TextField(max_length=20)
+#	group_size = models.IntegerField()
+
+
