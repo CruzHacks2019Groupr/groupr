765f6cc8991e5eed4f74925c6ccc81781443d598
Eric Vin <ericvin@outlook.com>
2019-01-19 14:45:40 -0800

    Cleaned up repo

--- project/functions/matcher.py
+++ project/functions/matcher.py
@@ -1,23 +1,15 @@
 #import everything we need
 import networkx as nx
 import itertools
+from Event import Event 
 
-""" #OBSOLETE, just add edge directly to Event
-def newSwipe(ID_1, ID_2) 												   #Update swipe occurs, giving a first and second ID
-	#DiG is digraph from database
-	#G is undirected graph from database
-	DiG.add_edge(ID_1, ID_2)
-	if ID_1 in DiG.neighbors(ID_2)
-		G.add_edge(ID_1, ID_2)
-	List = cliques_containing_node(G, nodes=ID_1)						
+def findPerfectGroup(Graph,ID_1,ID_2) 												#Update swipe occurs, giving a first and second ID
+	List = cliques_containing_node(Graph.G, nodes=[ID_1,ID_2])						
 	for j in List:															#Checks the size of each clique, ignore if not of size n
 		if len(j) is n
 			for i in j:														#removes nodes from the clique
-				G.remove_node(i)
-				DiG.remove_node(i)
-			break
-			#List[0] is added to the database
-"""
+				Graph.remove_user(i)
+			return j; 															#break when the first valid clique is found
 
 def forceGroups() #Makes groups based on current criterium
 
