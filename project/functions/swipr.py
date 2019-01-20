8b1bf111f8983a0c5cc1fbc00f2870f7b792a915
Conrad <waterflame99@gmail.com>
2019-01-19 21:23:26 -0800

    hook it upp

--- project/functions/swipr.py
+++ project/functions/swipr.py
@@ -6,14 +6,22 @@ EVENT_ID = 1
 
 def makeConnection(user1, user2):
 
-	print("Connection made")
+	print("Making Connection Between ", str(user1), " and ", str(user2))
 	print(EventModel.objects.all())
 	obj = EventModel.objects.get(id=EVENT_ID)
 	print(obj.name)
+
+
+"""
+	obj.getEdges
+	obj.setEdges
+	obj.getNodes
+	obj.setNodes
+	"""
 
 	#cool so obj is the event database object
 
 	#time to make shit more confusing
 
-	myEvent = Event()
+	#myEvent = Event()
 	
