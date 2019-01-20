2baa485c86b5d6485ff80def16e5add6aad4f084
Nicolas Menand <32970595+NMenand@users.noreply.github.com>
2019-01-19 16:38:31 -0800

    Update reccomend.py
    Updated to work with Strings for userID

--- project/functions/reccomend.py
+++ project/functions/reccomend.py
@@ -1,14 +1,15 @@
 #TODO Import Database interface 
 def reccomendNext(userID):
 	e = Event():
-	x=useron(userID):
-	if x is userID
-		if x+1 < e.groupSize
-			useron(userID)+=1:
-			return useron(userID):
-		else return -1:
-	else
-		useron(userID)+=1:
-		return x:
-
-
+	x = e.users.index(userID):
+	y = e.userOn(x):
+	if y>=e.groupSize
+		return -1:
+	if y is userID
+		if y+1>=e.groupSize
+			return -1:
+		e.userOn(x)+=1:
+		return y+1:
+	else 
+		e.userOn(x)+=1:
+		return y:
