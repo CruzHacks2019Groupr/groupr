393ae8c50f9b300d7e2c70ca49a58ed3fb6c7cde
Kevin Pinney <popocola13@gmail.com>
2019-01-19 16:24:21 -0800

    Revert "Update Event.py"
    This reverts commit 4e7c1be5075085b47196de3b88bccfe0d3fa0368.

--- project/functions/Event.py
+++ project/functions/Event.py
@@ -11,17 +11,14 @@ class Event:
 	def __init__(self):
 		self.DG = nx.DiGraph()
 		self.G = nx.Graph()
-		self.userList = [] #we should have an ordered node list in order to make sure users don't have repeats
 
 	def add_user(self, userId):
 		self.DG.add_node(userId)
 		self.G.add_node(userId)
-		self.userlist.append(userId)
 
 	def remove_user(self, userId):
 		self.DG.remove_node(userId)
 		self.G.remove_node(userId)
-		self.userList.remove(userId)
 
 	def add_edge(self, sourceUser, destinationUser):
 		self.DG.add_edge(sourceUser, destinationUser)
@@ -32,15 +29,12 @@ class Event:
 
 	#Returns list of ints
 	def export_users(self):
-		'''
 		nodesList = list(self.DG.nodes(self.DG))
 
 		for x in range(len(nodesList)):
 			nodesList[x] = nodesList[x][0]
 
 		return nodesList
-		'''
-		return self.userList
 
 	#Returns list of int tuples
 	def export_edges_directed(self):
