75b2cbdd9a80f379343faedb2990eec7873ae73b
Eric Vin <ericvin@outlook.com>
2019-01-19 21:40:34 -0800

    Something

--- project/functions/swipr.py
+++ project/functions/swipr.py
@@ -9,6 +9,27 @@ def makeConnection(user1, user2):
 	print("Making Connection Between ", str(user1), " and ", str(user2))
 	print(EventModel.objects.all())
 	obj = EventModel.objects.get(id=EVENT_ID)
+
+	#Creates Event object from EventModel
+	testEvent = Event(obj.di.getNodes(), obj.di.getEdges())
+
+	for x in range(1,21):
+		testEvent.add_user(x);
+
+	testEvent.add_edge(user1, user2)
+
+	#DEBUG#
+	print("Graph dump:")
+	print(testEvent.export_users())
+	print(testEvent.export_edges_directed())
+	print(testEvent.export_edges_undirected())
+	print("")
+
+	obj.di.setNodes(testEvent.export_users());
+	obj.di.setEdges(testEvent.export_edges_directed());
+
+	obj.save();
+
 	print(obj.name)
 
 
