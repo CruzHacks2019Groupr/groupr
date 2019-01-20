44a7f2d6813ef08107d869a6c4f06d91167597e6
Luke Harvey <L@Lukes-MacBook-Pro-3.local>
2019-01-19 17:01:56 -0800

    Fixed Event and Graph

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import models.graph as Graph
 #import networkx as nx
 
 
@@ -35,29 +36,3 @@ class Event(models.Model):
 
 	def getUsersOn():
 		a = userson.split(' ')
-
-
-
-
-class Graph(models.Model):
-	nodes = models.CharField(max_length=500)
-	edges_a = models.CharField(max_length=5000)
-	edges_b = models.CharField(max_length=5000)
-	def __str__(self):
-		pass
-	def getEdges(self):
-		a = [int(i) for i in self.edges_a.split(' ')]
-		b = [int(i) for i in self.edges_b.split(' ')]
-		return list(zip(a, b))
-
-	def setEdges(self, edges):
-		(a, b) = list(zip(*edges))
-
-		self.edges_a = ' '.join(map(str,a))
-		self.edges_b = ' '.join(map(str,b))
-
-	def getNodes(self):
-		return list([int(i) for i in self.nodes.split(' ')])
-
-	def setNodes(self, nodes):
-		self.nodes = ' '.join(map(str,nodes))
