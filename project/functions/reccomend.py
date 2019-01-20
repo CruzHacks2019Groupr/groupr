ec062c2e434f1b07c2998e1d3b0f2f4ca0172c55
Nicolas Menand <32970595+NMenand@users.noreply.github.com>
2019-01-20 04:48:17 -0800

    Update reccomend.py
    added edge case

--- project/functions/reccomend.py
+++ project/functions/reccomend.py
@@ -4,14 +4,17 @@ from project.models import Event
 def reccomendNext(e,userID):
 	x = e.getUsers().index(userID)					#loads user index by searching for userID's position
 	UserList = e.getUsersOn()
-	y = UserList[x]							#gets int at user index representing the next user to swipe on
-	if x == y:									#checks to make sure the user doesn't swipe on themselves
+	y = UserList[x]
+	if x = 0 and y =0								#edge case
+		x+=1		
+													#gets int at user index representing the next user to swipe on
+	if x == y:										#checks to make sure the user doesn't swipe on themselves
 		y+=1
-		if y>=len(e.getUsers()):							#checks to see if the position of the user is valid
+		if y>=len(e.getUsers()):					#checks to see if the position of the user is valid
 			return -1
 		e.setUserOn(x,y)							#increment the userID and reloads it
 		y = e.getUsersOn().index(x)
-	if y>=len(e.getUsers()):							#checks to see if the position of the user is valid
+	if y>=len(e.getUsers()):						#checks to see if the position of the user is valid
 		return -1
 	else: 
 		e.setUserOn(x,y+1)							#increments the value for the future
