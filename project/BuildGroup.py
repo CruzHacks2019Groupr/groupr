82d8535a71c19ab1d8aa707763c8e82294c81172
Luke Harvey <L@Lukes-MacBook-Pro-3.local>
2019-01-20 00:50:32 -0800

    BuildGroup saves Groups

--- project/BuildGroup.py
+++ project/BuildGroup.py
@@ -2,8 +2,10 @@ import networkx as nx
 import itertools
 from .models import Groups 
 
-def buildGroup(GroupList)
+def buildGroup(EventID, GroupList)
 	g = Groups():
 	for i in GroupList
 		g.addUser(i)
+	g.linkedEventId = EventID
+	g.save()
 	return g
