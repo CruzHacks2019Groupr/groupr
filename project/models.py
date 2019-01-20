8ac699cac495821f3beb6029c8501188466d4237
Luke Harvey <L@Lukes-MacBook-Pro-3.local>
2019-01-19 18:38:10 -0800

    Fixed Event and Graph

--- project/models.py
+++ project/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import models.graph as Graph
+#import project.models_.graph as Graph
 #import networkx as nx
 
 
@@ -16,36 +16,63 @@ class Test(models.Model):
 #	birth_date = models.DateField(null=True, blank=True)
 #	pic = models.ImageField(null=True)
 
+class Graph(models.Model):
+	nodes = models.CharField(max_length=500)
+	edges_a = models.CharField(max_length=5000)
+	edges_b = models.CharField(max_length=5000)
+	def __str__(self):
+		pass
+	def getEdges(self):
+		a = [int(i) for i in self.edges_a.split(' ')]
+		b = [int(i) for i in self.edges_b.split(' ')]
+		return list(zip(a, b))
+
+	def setEdges(self, edges):
+		(a, b) = list(zip(*edges))
+
+		self.edges_a = ' '.join(map(str,a))
+		self.edges_b = ' '.join(map(str,b))
+
+	def getNodes(self):
+		return list([int(i) for i in self.nodes.split(' ')])
+
+	def setNodes(self, nodes):
+		self.nodes = ' '.join(map(str,nodes))
+
 class Event(models.Model):
 	name = models.TextField(max_length=50)
 	group_size = models.IntegerField()
 	di = models.OneToOneField(
         Graph,
         on_delete=models.PROTECT,
+        related_name = 'di',
     )
 	undi = models.OneToOneField(
         Graph,
         on_delete=models.PROTECT,
+        related_name = 'undi',
     )
-	users = models.CharField(max_length=5000, default='')
+	users = models.CharField(max_length=5000, default="", blank = True) #optional
 	creator = models.CharField(max_length=150, blank=False)
-	userson = models.CharField(max_length=5000, default='')
+	userson = models.CharField(max_length=5000, default="", blank = True) #optional
 
-	def getUsers():
-		a = users.split(' ')
+	def getUsers(self):
+		a = self.users.split(' ')
+		return a
 
-	def getUsersOn():
-		a = userson.split(' ')
+	def getUsersOn(self):
+		a = self.userson.split(' ')
+		return a
 
-	def setUserOn(pos,num):
-		a = userson.split(' ')
+	def setUserOn(self, pos, num):
+		a = self.userson.split(' ')
 		a[pos] = num
-		userson = ' '.join(a)
+		self.userson = ' '.join(str(x) for x in a)
 
-	def addUser(id):
-		if len(users) == 0:
-			users = users + id
-			userson = userson + 0
+	def addUser(self, id):
+		if len(self.users) == 0:
+			self.users = self.users + id
+			self.userson = self.userson + str(0)
 		else:
-			users = users + " " + id
-			userson = userson + " " + 0
+			self.users = self.users + " " + id
+			self.userson = self.userson + " " + str(0)
