ed0350ae9f9f233d5f766dce7bf9552712f1154e
Kevin Pinney <popocola13@gmail.com>
2019-01-19 18:10:53 -0800

    Debugged.  Should work fine :)

--- project/functions/matcher.py
+++ project/functions/matcher.py
@@ -3,51 +3,53 @@ import networkx as nx
 import itertools
 from Event import Event 
 
-def findPerfectGroup(Event, node, size):									   #Update swipe occurs, giving a first and second ID
+def findPerfectGroup(Event, node, size):								   						#Update swipe occurs, giving a first and second ID
 	List = nx.cliques_containing_node(Event.G, node)						
-	for j in List:														   #Checks the size of each clique, ignore if not of size n
+	for j in List:														  					 	#Checks the size of each clique, ignore if not of size n
 		if len(j) is size:
-			for i in j:													   #removes nodes from the clique
+			for i in j:													   						#removes nodes from the clique
 				Event.remove_user(i)
-			return j; 													   #break when the first valid clique is found
+			return j; 													   						#break when the first valid clique is found
 
-def forceGroups(Event, size): #Makes groups based on current criterium
+def forceGroups(Event, size):								 									#Makes groups based on current criterium
 
-	possibleGroups = list(itertools.combinations(list(Event.DG), size))        #All combinations of the Node list in groups of length n are stored in list
+	possibleGroups = list(itertools.combinations(list(Event.DG), size))    						#All combinations of the Node list in groups of length n are stored in list
 	i = 0
-																		   #Nested loops build a combination of groups (disjoint is used to make sure the same person doesnt end up in a group twice)
-	while not set(possibleGroups[0]).isdisjoint(possibleGroups[i]):
-		groupCombo[i].append(possibleGroups[i])                            #Add first group
-		scoreList[i] += computeScore(possibleGroups[i])                    #Add score of first group
+	groupCombo = [list(list())]	
+	scoreList = []										  										#Nested loops build a combination of groups (disjoint is used to make sure the same person doesnt end up in a group twice)
+	while i < len(possibleGroups) and not set(possibleGroups[0]).isdisjoint(possibleGroups[i]):
+		groupCombo.append([])
+		groupCombo[i].append(possibleGroups[i])                         						#Add first group
+		scoreList.append(computeScore(Event, possibleGroups[i]))           						#Add score of first group
 		j = i + 1
 		while j < len(possibleGroups):
-			for k in groupCombo[i]:                                        #Checks if any shared members in current running groupCombo
-				if not set(groupCombo[i][k]).isdisjoint(possibleGroups[j]):
-					break												   #Breaks if shared members
-				groupCombo[i].append(possibleGroups[j])                    #If there is no break then group is added to combo
-				scoreList[i] += computeScore(possibleGroups[j])            #Score is added as well
+			for k in groupCombo[i]:                                        						#Checks if any shared members in current running groupCombo
+				if not set(k).isdisjoint(possibleGroups[j]):
+					break												   						#Breaks if shared members
+				groupCombo[i].append(possibleGroups[j])                    						#If there is no break then group is added to combo
+				scoreList[i] += computeScore(Event, possibleGroups[j])            				#Score is added as well
 			j+=1
 		i+=1
 
 	BestIndex = 0
-	for k in scoreList:													   #Finds best group combo (highest score)
+	for k in range(len(scoreList)):													   			#Finds best group combo (highest score)
 		if scoreList[k] > scoreList[BestIndex]:
 			BestIndex = k
 
-	for k in groupCombo[BestIndex]:                                        #Removes groups from graph
-		for l in groupCombo[BestIndex][k]:								   
-			Event.DG.remove_user(groupCombo[BestIndex][k][l])
+	for k in range(len(groupCombo[BestIndex])):                                        			#Removes groups from graph
+		for l in range(len(groupCombo[BestIndex][k])):								   
+			Event.remove_user(groupCombo[BestIndex][k][l])
 
-	groupCombo[BestIndex].append(list(Event.DG))							   #All remaining people are put into a group
+	groupCombo[BestIndex].append(list(Event.DG))							   					#All remaining people are put into a group
 
 	return groupCombo[BestIndex]
 
 	
-def computeScore(L):													   #For a given group computes the score, which is the number edges
+def computeScore(Event, L):													   					#For a given group computes the score, which is the number edges
 
 	score = 0
 	for i in L:
 		for j in L:
-			if L[i] in DiG.neighbors(L[j]):
+			if i != j and i in Event.DG.neighbors(j):
 				score+=1;
 	return score
