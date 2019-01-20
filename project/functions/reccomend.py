0ec4380c1f59514ecfc19bac3a7ec60243a4911d
Nicolas Menand <32970595+NMenand@users.noreply.github.com>
2019-01-19 20:25:57 -0800

    Fixed reccomend.py
    Fixed recommend.py and tried a test function

--- project/functions/reccomend.py
+++ project/functions/reccomend.py
@@ -1,17 +1,18 @@
 from project.models import Graph
 from project.models import Event
 
-def reccomendNext(userID):
-	e = Event()										#loads event Data type
-	x = e.getUsers.index(userID)					#loads user index by searching for userID's position
-	y = e.getUserOn(x)								#gets int at user index representing the next user to swipe on
-	
-	if y is userID:									#checks to make sure the user doesn't swipe on themselves
-		e.setUserOn(x,y+1)							#increment the userID and reloads it
-		y = e.getUserOn(x)
-
-	if y>=len(e.getUsers):							#checks to see if the position of the user is valid
+def reccomendNext(e,userID):
+	x = e.getUsers().index(userID)					#loads user index by searching for userID's position
+	UserList = e.getUsersOn()
+	y = UserList[x]							#gets int at user index representing the next user to swipe on
+	if x == y:									#checks to make sure the user doesn't swipe on themselves
+		y+=1
+		if y>=len(e.getUsers()):							#checks to see if the position of the user is valid
+			return -1
+		e.setUserOn(x,y)							#increment the userID and reloads it
+		y = e.getUsersOn().index(x)
+	if y>=len(e.getUsers()):							#checks to see if the position of the user is valid
 		return -1
 	else: 
 		e.setUserOn(x,y+1)							#increments the value for the future
-		return e.getUsers(y)
+		return e.getUsers()[y]
