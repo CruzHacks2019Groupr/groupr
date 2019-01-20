493309c9c7ef5eceac9c15b62bbbd952a060434c
Nicolas Menand <32970595+NMenand@users.noreply.github.com>
2019-01-19 16:19:33 -0800

    Create reccomend.py
    Added Reccomender Function

---
+++ project/functions/reccomend.py
@@ -0,0 +1,14 @@
+#TODO Import Database interface 
+def reccomendNext(userID):
+	e = Event():
+	x=useron(userID):
+	if x is userID
+		if x+1 < e.groupSize
+			useron(userID)+=1:
+			return useron(userID):
+		else return -1:
+	else
+		useron(userID)+=1:
+		return x:
+
+
