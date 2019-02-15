d871a51707d287055cd37ba7a3e869622359ba1a
Conrad <conrad.menchine@gmail.com>
2019-02-14 21:07:51 -0800

    quick change

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$q(iq&n(uidm$&&5g^2ixzr37nqzm^(*96t3wh&rd#ie)m9y$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_ROOT = "project"
 
